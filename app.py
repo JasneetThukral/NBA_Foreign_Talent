fe4513f30831a517a82f9073012d7fa43aa8b613
wajid <wajidsid123@gmail.com>
2020-07-25 18:10:49 -0500

    flaskkk

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_USER'] = 'sql3356970'
+app.config['MYSQL_PASSWORD'] = 'gAgxITG1pb'
+app.config['MYSQL_HOST'] = 'sql3.freemysqlhosting.net'
+app.config['MYSQL_DB'] = 'sql3356970'
+app.config['MYSQL_CURSURCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor()
+    return 'Done!'
