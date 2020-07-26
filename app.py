b375960f59f76e3117ca00ee22b6643a3cd2c1c4
Neil Reddy <neilr3@illinois.edu>
2020-07-25 21:53:09 -0500

    setting up db on flask

--- app.py
+++ app.py
@@ -11,6 +11,26 @@ app.config['MYSQL_CURSURCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
+class Players(db.Model):
+    PlayerID = db.Column(db.Integer, primary_key=True)
+    PlayerName = db.Column(db.String(550), nullable=False)
+    TeamName = db.Column(db.String(300), db.ForeignKey(Teams.TeamName), nullable=False)
+    Positions = db.Column(db.String(10), nullable=False)
+    Height = db.Column(db.Integer, nullable=False)
+    Weight = db.Column(db.Integer, nullable=False)
+
+class Teams(db.Model):
+    pass
+
+class Statistics(db.Model):
+    PlayerID = db.Column(db.Integer, db.ForeignKey(Players.PlayerID))
+    numGames = db.Column(db.Integer, nullable=False)
+    
+
+class Leagues(db.Model):
+    LeagueName = db.Column(db.String(250), unique=True, nullable=False)
+    NumGames = db.Column(db.Integer)
+
 @app.route('/')
 def index():
     cur = mysql.connection.cursor()
