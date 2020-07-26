3457a95e46d9bcba1d9d6854ced6b611ac45accc
wajid <wajidsid123@gmail.com>
2020-07-26 02:52:00 -0500

    done

--- app.py
+++ app.py
@@ -33,11 +33,19 @@ def index():
 @app.route('/results',methods=['GET', 'POST'])
 def script_output():
     cur = mysql.connection.cursor()
+    cur_two = mysql.connection.cursor()
+    cur_three = mysql.connection.cursor()
+    cur_four = mysql.connection.cursor()
     # if str(table) == "Players":
-    print(table)
-    cur.execute("SELECT * FROM Players")
+    output_one = cur.execute("SELECT * FROM Players")
+    output_two = cur_two.execute("SELECT * FROM Statistics")
+    output_three = cur_three.execute("SELECT * FROM Leagues")
+    output_four = cur_four.execute("SELECT * FROM Teams")
     rows = cur.fetchall()
+    row_two = cur_two.fetchall()
+    row_three = cur_three.fetchall()
+    row_four = cur_four.fetchall()
     print(rows)
-    return render_template('results.html',output=rows)
+    return render_template('results.html',output_one=rows,output_two=row_two,output_three=row_three,output_four=row_four)
 if __name__ == 'main':
     app.run(debug=True)
