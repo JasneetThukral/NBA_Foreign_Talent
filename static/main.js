948c414bbb14364efcb5d1a4c137e71bef6fb364
WajidS <42981621+WajidS@users.noreply.github.com>
2020-08-03 04:32:30 -0500

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,11 +1,50 @@
-
 // removing player from table
+//https://www.fourfront.us/blog/store-html-table-data-to-javascript-array/
+var TableData = new Array();
+$('#add_table').each(function(row, tr){
+    TableData[row]={
+        "PlayerID" : $(tr).find('td:eq(0)').text()
+        , "PlayerName" :$(tr).find('td:eq(1)').text()
+        , "Points" : $(tr).find('td:eq(2)').text()
+        , "Assists" : $(tr).find('td:eq(3)').text()
+        ,"Rebounds" : $(tr).find('td:eq(3)').text()
+    }
+});
+
+    
+// function addval(row) {
+// $('#add_table').each(function(tr){
+//     var TableData = new Array();
+//     TableData[row]={
+//         "PlayerId" : $(tr).find('td:eq(0)').text()
+//         , "PlayerName" :$(tr).find('td:eq(1)').text()
+//         , "Points" : $(tr).find('td:eq(2)').text()
+//         , "Assists" : $(tr).find('td:eq(3)').text()
+//         ,"Rebounds" : $(tr).find('td:eq(4)').text(),
+//         "random" : $(tr).find('td:eq(5)').text()
+//     }
+// });
+//     return Table_Data;
+// }
+function Insert_Data() {
+    var table = document.getElementById("del_table");
+    var tr="";
+    TableData.forEach(x=>{
+       tr+='<tr>';
+       tr+='<td>'+x.PlayerID+'</td>'+'<td>'+x.PlayerName+'</td>'+'<td>'+x.Points+'</td>'+'<td>'+x.Assists+'</td>'+'<td>'+x.Rebounds+'</td>'
+       tr+='</tr>'
+  
+    })
+    table.innerHTML+=tr;
+    //Help......  
+  } 
+//  TableData.shift();  // first row is the table header - so remove
 
 function delrow() {
     var index, table = document.getElementById('del_table');
       for(var i = 1; i < table.rows.length; i++)
       {
-          table.rows[i].cells[2].onclick = function()
+          table.rows[i].cells[5].onclick = function()
           {
               var c = confirm("Do you want to remove this player?");
               if(c === true)
@@ -21,15 +60,18 @@ function delrow() {
     }
     function addr() {
         var add_table = document.getElementById('add_table');
-        var del_table = document.getElementById('del_table');
           for(var i = 1; i < add_table.rows.length; i++)
           {
-              add_table.rows[i].cells[2].onclick = function()
+              add_table.rows[i].cells[5].onclick = function()
               {
                   var c = confirm("Do you want to add this player?");
                   if(c === true)
                   {
-                      del_table.addRow(add_table.rows.length);
+                      if (del_table.rows.length > 14) {
+                          alert("Your team roster must be under 14 people. Please remove the neccessary players to proceed");
+                      } else {
+                      Insert_Data();
+                      }
                   }
                   
                   //console.log(index);
@@ -57,8 +99,5 @@ function delrow() {
     //       };
           
     //     }
-    
-    
-    
     
     
